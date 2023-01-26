7ce38e89ce8f9e41ea2911a773c59aec01d878a0
aribelruiz <94211795+aribelruiz@users.noreply.github.com>
2023-01-25 23:22:38 -0500

    Non-Optimized solution to Sieve in Parallel

--- Assigment1/Assignment1.java
+++ Assigment1/Assignment1.java
@@ -8,70 +8,117 @@
 //      This is an introductory assignment to multi-threaded programming.
 
 import java.util.*;
+import java.text.NumberFormat;
+import java.text.DecimalFormat;
 
 public class Assignment1 
 {
+    // Variables for thread management
     public static int threadsRunning = 0;
+    public static boolean executionDone = false; 
 
-    // Creating a Hash Set storing primes and a Hash Set storing composites. 
+    // Creates a Hash Set storing primes and a Hash Set storing composites
     public static HashSet<Long> primeList = new HashSet<Long>();
     public static HashSet<Long> compositeList = new HashSet<Long>();
+
+    // Variables for sum and prime count
+    public static long primeSum = 0;
+    public static long compSum = 0;
+    public static long numOfPrimes = 0;
 
     // Creates a boolean array that checks how many threads are running
     public static boolean threadParking[] = new boolean[8];
 
-
     public static void main(String[] args)
     {
+        // Starts timer
+        long start = System.nanoTime();
+
+        // Variable alongside FindOpenIndex() assists in checking if new thread can be started
         int parkingIndex;
 
-        for (int i = 0; i < 1000; i++) 
+        // Initializes prime and composite lists (not including 0 or 1)
+        // for (long i = 2; i <= Math.pow(10,8); i++)
+        for (long i = 2; i <= 50; i++)
         {
-            // Resetting Parking Index variable
-            parkingIndex = -1;
+            primeList.add(i);
+        }
 
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e){
-            }
+        // Checks if program done (executionDone set to true in thread when upper bound is reached)
+        while (!executionDone)
+        {
+            // Resets parking index variable
+            parkingIndex = -1;
 
             // Checks if there is an opening for a new thread
             if ((parkingIndex = FindOpenIndex()) != -1)
             {
-                System.out.println("CREATING NEW THREAD @ " + (parkingIndex) + ", thread count: " + threadsRunning);
-
+                // Creating new thread at (parkingIndex)
                 MultiThreadThing myThing = new MultiThreadThing(parkingIndex);
                 Thread myThread = new Thread(myThing);
                 myThread.start();
             }
-            else
-            {
-                System.out.println("CONTINUE");
-            }
         }
 
-        System.out.println("DONE");
+        // Prints out information for Primes
+        System.out.println("\nPRIMES: " + primeList);
+        System.out.println("PRIME SUM: " + sum(primeList));
+        System.out.println("# of PRIMES: " + numOfPrimes);
+        // System.out.println("PRIME SUM: " + primeSum);
+        // System.out.println("# of PRIMES: " + primeList.size());
+
+        
+        // Prints out information for Composites
+        System.out.println("\nCOMPOSITES: " + compositeList);
+        System.out.println("COMP SUM: " + sum(compositeList));
+        // System.out.println("COMP SUM: " + compSum);
+
+
+        // Ends Timer
+        long end = System.nanoTime();
+
+        // Prints execution time
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.println("\nExecution time is " + formatter.format((end - start) / 1000000000d) + " seconds\n");
+
     }
 
-
+    // Searches for open index to start thread
     public static int FindOpenIndex()
     {
-        System.out.println("\nFINDING PARKING");
+        // Checks if index is open to start new thread
         for (int i = 0; i < threadParking.length; i++)
         {
-            // If thread parking has an opening, return index of opening
+            // If thread parking has an opening, returns index of opening
             if (threadParking[i] == false)
             {
-                System.out.println("Parking at index: " + i);
+                // Found Parking at index i
                 threadParking[i] = true;
                 return i;
             }
         }
-
         // If there are no openings in thread parking, return -1.
-        System.out.println("No Parking.");
         return -1;
     }
+
+    // Calculates the sum of the elements in a hash set
+    public static long sum(HashSet<Long> sumList) 
+    {
+        long sum = 0;
+
+        // Creates Iterator object.
+        Iterator itr = sumList.iterator();
+
+        // Sums all numbers in list and increments number of primes counter
+        while (itr.hasNext()) 
+        {
+            sum += (long)itr.next();
+            numOfPrimes++;  // remove
+        }
+
+        return sum;
+    }
+
 }
 
 
--- Assigment1/MultiThreadThing.java
+++ Assigment1/MultiThreadThing.java
@@ -10,22 +10,62 @@ public class MultiThreadThing implements Runnable {
     @Override
     public synchronized void run() {
 
-        // Increment number of threads running
-        Assignment1.threadsRunning++;
-        System.out.println("THREAD " + threadNumber + " START, thread count: "+ Assignment1.threadsRunning);
+        // long upperBound = (long)Math.pow(10,8);
+        long upperBound = 50;   // used for smaller testing
         
-        for (int i = 1; i <= 10; i++)
+        // Increment number of threads running
+        // System.out.println("THREAD " + threadNumber + " STARTED, thread count: "+ Assignment1.threadsRunning);
+        Assignment1.threadsRunning++;
+        
+        long num = 1;
+        for (long i = 2; i <= upperBound; i++)
         {
-            // Sleeping just for readability when testing
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e){
+            if (Assignment1.primeList.contains(i) == true && Assignment1.compositeList.contains(i) == false)
+            {
+                // Calculates numbers with factor of i.
+                if (i <= (long)Math.floor(Math.sqrt(upperBound)))
+                {
+                    // Starts at i^2 when adding to list of composites.
+                    num = (long)Math.pow(i,2);
+                    while (num <= upperBound)
+                    {                    
+                        // If num is not in compositeList, add to compositeList and remove from primeList
+                        if (Assignment1.compositeList.contains(num) == false )
+                        {
+                            // Adds num to composite list and removes num from primeList
+                            Assignment1.compositeList.add(num);
+                            Assignment1.primeList.remove(num);
+                                                        
+                            // Assignment1.compSum += num;
+                            // Assignment1.primeSum -= num;
+                        }
+                        num = num + i;
+                    }
+                    
+                    // ==========================TO BE ALTERED======================================
+                    // // Adds primes to prime list
+                    // if (Assignment1.compositeList.contains(i) == false)
+                    // {
+                    //     // Adding i to prime list
+                    //     // System.out.println("Adding PRIME " + i + " from THREAD " + threadNumber);
+                    //     // Assignment1.primeList.add(i);
+                    //     // Assignment1.primeSum += i;
+                    //     break;
+                    // }
+                    // ==========================TO BE ALTERED======================================
+
+                }
+                else
+                {
+                    Assignment1.executionDone = true;
+                    break;
+                }
             }
         }
 
-        // Updating threads running
+        // Updates threads running
         Assignment1.threadParking[threadNumber] = false;
         Assignment1.threadsRunning--;
-        System.out.println("THREAD " + threadNumber + " FINISHED, thread count: "+ Assignment1.threadsRunning);
+        // System.out.println("THREAD " + threadNumber + " FINISHED, thread count: "+ Assignment1.threadsRunning);
     }
 }
