cd1cc70a736ea2acbbdce4c4771a1da4be0d2492
aribelruiz <94211795+aribelruiz@users.noreply.github.com>
2023-01-26 16:22:16 -0500

    Added printing required output to primes.txt file

--- Assigment1/Assignment1.java
+++ Assigment1/Assignment1.java
@@ -8,6 +8,9 @@
 //      This is an introductory assignment to multi-threaded programming.
 
 import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.NumberFormat;
 import java.text.DecimalFormat;
 
@@ -27,19 +30,21 @@ public class Assignment1
     public static int upperBound = (int)Math.pow(10,8);
 
     // Variables for prime numbers
-    public static HashSet<Long> primeList = new HashSet<Long>();
-    public static long numOfPrimes = 0;
+    private static long[] top10Primes = new long[10];
+    private static int top10Counter = 0;
+    private static long numOfPrimes = 0;
+    private static long primeSum = 0; 
+  
 
     // Boolean list storing whether each number is composite (number N represented by index N)
     public static boolean[] isCompositeList = new boolean[upperBound + 1]; 
-
 
     // ========================================= Functions =========================================
     public static void main(String[] args)
     {
         // Starts timer
         long start = System.nanoTime();
-
+   
         // Checks for program finish (executionDone set to true in thread when upperBound reached)
         while (!executionDone)
         {
@@ -56,26 +61,61 @@ public class Assignment1
             }
         }  
 
-        // Adds numbers into prime list that are not in composite list (excluding 0 and 1)
-        for (long i = 2; i <= upperBound; i++)
+        // Finds prime numbers (number is prime if not found in composite list (excluding 0 and 1))
+        for (long i = upperBound; i >= 2; i--)
         {
             if (isCompositeList[(int)i] == false)
             {
-                Assignment1.primeList.add(i);
+                // Assures top 10 primes are added to top10Primes[]
+                if (top10Counter < 10)
+                    top10Primes[top10Counter++] = i;
+
+                // Increments number of primes and adds primes to primeSum
+                numOfPrimes++;
+                primeSum += i;
             }
         }
 
-        // Prints out information for prime numbers
-        System.out.println("\nPRIME SUM: " + sumPrimes(primeList));
-        System.out.println("# of PRIMES: " + numOfPrimes);
-      
+        // Creates primes.txt file to print output
+        try {
+            File outputFile = new File("primes.txt");
+        
+            if (outputFile.createNewFile())
+                System.out.println("File created: " + outputFile.getName());
+            else
+                System.out.println("File '" + outputFile.getName() + "' already exists.");
+
+        } catch (IOException e) {
+            System.out.println("An error occured.");
+            e.printStackTrace();
+        }
+
         // Ends timer
         long end = System.nanoTime();
 
         // Prints execution time
         NumberFormat formatter = new DecimalFormat("#0.00000");
-        System.out.println("\nExecution time is " + formatter.format((end - start) / 1000000000d) + " seconds\n");
 
+        // Writes prime number information to prime.txt file
+        try {
+            FileWriter myWriter = new FileWriter("primes.txt");
+            
+            // Printing <execution time (in seconds)> <total number of primes found> <sum of all primes found> to output 
+            myWriter.write(formatter.format((end - start) / 1000000000d) + "s ");
+            
+            myWriter.write(numOfPrimes + " ");
+            myWriter.write(primeSum + " ");
+
+            // Printing <top 10 maximum primes, listed in order from lowest to highest> to output 
+            myWriter.write("\n" + printTop10Primes(top10Primes));
+            
+            // Closing file writer
+            myWriter.close();
+            System.out.println("Successfully wrote output to 'primes.txt'.\n");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
     }
 
     // Function searches for open index to start new thread
@@ -96,20 +136,24 @@ public class Assignment1
         return -1;
     }
 
-    // Function calculates the sum of the elements in a hash set of type 'Long'
-    public static long sumPrimes(HashSet<Long> sumList) 
+    // Function returns the top 10 prime numbers as a string (from lowest to highest)
+    public static String printTop10Primes(long[] top10)
     {
-        long sum = 0;
-        Iterator itr = sumList.iterator();
-
-        // Sums all numbers in list and increments total numofPrimes counter
-        while (itr.hasNext()) 
+        String top10Str = "";
+        top10Str += "[";
+        for (int i = top10.length - 1; i >= 0; i--)
         {
-            sum += (long)itr.next();
-            numOfPrimes++;
-        }
+            if (i == 0)
+            {
+                top10Str += (top10[i]);
+                break;
+            }
 
-        return sum;
+            top10Str += (top10[i] + ", ");
+        }
+        top10Str += "]\n";
+
+        return top10Str;
     }
 }
 
